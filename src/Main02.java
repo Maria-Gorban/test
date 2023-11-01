4460beddbb62f5039bfd0b957a6636f325c7341a
Maria G <mgorban@rheinahrcampus.de>
2023-11-01 19:11:10 +0100

    add some code

---
+++ src/Main02.java
@@ -0,0 +1,16 @@
+public class Main02 {
+  public static int concatArray(int[] arr) { //метод для объединения
+    int result = 0;
+    for (int i = 0, j = arr.length - 1; i < arr.length; i++, j--) {
+      result += arr[i] * Math.pow(10, j); //10^j
+    }
+    return result;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = {3, 4, 7, 8, 2};
+    System.out.println(concatArray(arr));
+  }
+}
+
+
